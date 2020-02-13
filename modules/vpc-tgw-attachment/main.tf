961050c01ee4d6cb13905ac769576ad2d71ab201
Matthijs Suringa <Matthijs.Suringa@gmail.com>
2020-02-13 16:25:34 +0000

    Created module for TGE attachment

---
+++ modules/vpc-tgw-attachment/main.tf
@@ -0,0 +1,32 @@
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = "${var.tfe_org_name}"
+    hostname     = "${var.tfe_host_name}"
+    workspaces = {
+      name = "${var.tfe_core_network_workspace_name}"
+    }
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
+  vpc_id             = "${var.vpc_id}"
+  transit_gateway_id = "${data.terraform_remote_state.network.outputs.transitgw}"
+  subnet_ids         = "${var.subnet_ids}"
+  dns_support        = "disable"
+
+  tags = {
+    Name = "tlz-tgw-attachment"
+  }
+}
+
+# resource "aws_route" "route" {
+#   count = length(var.route_tables)
+
+#   route_table_id = data.aws_route_table.rt.*.id[count.index]
+#   destination_cidr_block = "0.0.0.0/0"
+#   transit_gateway_id = data.aws_ec2_transit_gateway.tgw.id
+
+#   depends_on = [aws_ec2_transit_gateway_vpc_attachment.tgw_attachment]
+# }
---
+++ modules/vpc-tgw-attachment/variables.tf
@@ -0,0 +1,33 @@
+
+
+# variable "route_tables" {
+#   type        = list(string)
+#   description = "The list of route tables to add the TGW route to"
+# }
+
+variable "subnet_ids" {
+  type        = "list"
+  description = "The list of subnets to associate with the TGW, max one per AZ allowed"
+}
+
+variable "vpc_id" {
+  type        = "string"
+  description = "Name of the VPC"
+}
+
+variable "tfe_core_network_workspace_name" {
+  type        = "string"
+  description = "Name of network workspace"
+
+  default = "tlz-core_network"
+}
+
+variable "tfe_host_name" {
+  description = "host_name for ptfe"
+  default     = "prod.ptfe.dht.dev"
+}
+
+variable "tfe_org_name" {
+  description = "ptfe organization name"
+  default     = "example"
+}
