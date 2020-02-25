df85e01aa7a588ff3d9e385b0154d9759ec556bc
Darren Jackson <darren.k.j@gmail.com>
2020-02-25 17:14:01 +0000

    more TF11 to TF12 updates

--- modules/vpc-tgw-attachment/main.tf
+++ modules/vpc-tgw-attachment/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "network" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_attachment" {
   vpc_id             = var.vpc_id
-  transit_gateway_id = data.terraform_remote_state.network.transitgw
+  transit_gateway_id = data.terraform_remote_state.network.outputs.transitgw
   subnet_ids         = ["${var.subnet_ids}"]
   dns_support        = "disable"
 
